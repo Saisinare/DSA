d2048e0da2710038c36deb7f9fcc263932642410
unknown <saisinare19@gmail.com>
2024-12-13 13:52:21 +0530

    solve largest subarray with summation k problem

---
+++ 3. Solve the problems on Arrays/Easy/findLargestSubarrayWithSumK.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<vector>
+#include<map>
+
+using namespace std;
+
+int findLargestSubArrayWithSumK(vector<int> arr,int k){
+    long long sum = 0;
+    int maxLen =0;
+    map<long long,int> hashmap;
+    for(int i=0;i<arr.size();i++){
+        sum += arr[i];
+        if(sum==k){
+           maxLen = max(maxLen,i+1);
+        }
+        int rem = sum-k;
+        if(hashmap.find(rem)!= hashmap.end() ){
+            int len = i-hashmap[rem];
+            maxLen = max(len,maxLen);
+        }
+        hashmap[sum] = i;
+    }
+    return maxLen;
+}
+
+int main(){
+    vector<int> arr ={2,1,3,1,1};
+    int maxLenOfLargestKSumArray = findLargestSubArrayWithSumK(arr,3);
+    cout<<maxLenOfLargestKSumArray;
+    return 0;
+}
