10d656c174828dca53eda0321cd83b3ee82f0121
unknown <saisinare19@gmail.com>
2025-04-05 00:17:28 +0530

    merge sort fix

--- 2. Sorting/mergeSort.cpp
+++ 2. Sorting/mergeSort.cpp
@@ -3,50 +3,45 @@
 using namespace std;
 
 class Solution {
-public:
-    void merge(vector<int>& arr, int l, int mid, int r) {
+  public:
+    void merge(vector<int>& arr, int low, int mid, int high){
+        int left = low;
+        int right = mid+1;
         vector<int> temp;
-        int i = l;
-        int j = mid + 1;
-
-        // Merge two sorted subarrays
-        while (i <= mid && j <= r) {
-            if (arr[i] <= arr[j]) {
-                temp.push_back(arr[i]);
-                i++;
-            } else {
-                temp.push_back(arr[j]);
-                j++;
+        
+        while(left<=mid && right<= high){
+            if(arr[left] <= arr[right]){
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else{
+                temp.push_back(arr[right]);
+                right++;
             }
         }
-
-        // Copy remaining elements
-        while (i <= mid) {
-            temp.push_back(arr[i]);
-            i++;
+        while(left<=mid){
+            temp.push_back(arr[left]);
+            left++;
         }
-
-        while (j <= r) {
-            temp.push_back(arr[j]);
-            j++;
+        while(right<=high){
+            temp.push_back(arr[right]);
+            right++;
         }
-
-        // Copy sorted temp array to original array
-        for (int k = 0; k < temp.size(); k++) {
-            arr[l + k] = temp[k];
+        for(int i=low ;i<=high;i++){
+            arr[i] = temp[i-low];
         }
     }
-
+    
     void mergeSort(vector<int>& arr, int l, int r) {
-        if (l >= r) return;
-
-        int mid = l + (r - l) / 2;
-        mergeSort(arr, l, mid);
-        mergeSort(arr, mid + 1, r);
-        merge(arr, l, mid, r);
+        int mid = (l+r)/2;
+        if(l==r){
+            return;
+        }
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        merge(arr,l,mid,r);
     }
 };
-
 int main() {
     vector<int> arr = {5, 3, 8, 4, 2};
     Solution s;
