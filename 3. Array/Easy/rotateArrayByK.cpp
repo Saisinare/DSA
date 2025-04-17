42d17be9f71d8a299d6d239d58f7f684c3f7d9e8
unknown <saisinare19@gmail.com>
2025-04-17 23:23:58 +0530

    rotate Array By optimal Solution By K positions

--- 3. Array/Easy/rotateArrayByK.cpp
+++ 3. Array/Easy/rotateArrayByK.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 vector<int> rotateArray(vector<int> arr, int k) {
@@ -18,9 +19,18 @@ vector<int> rotateArray(vector<int> arr, int k) {
     return arr;
 }
 
+
+vector<int> rotateArrayOptimal(vector<int> arr, int k) {
+    int n = arr.size();
+    reverse(arr.begin(),arr.begin()+k);
+    reverse(arr.begin()+k,arr.begin()+n);
+
+    return arr;
+}
+
 int main(){
     vector<int> arr = {1,2,3,4};
-    vector<int> result = rotateArray(arr,1);
+    vector<int> result = rotateArrayOptimal(arr,1);
     for(int i=0;i<result.size();i++){
         cout<< result[i];
     }
