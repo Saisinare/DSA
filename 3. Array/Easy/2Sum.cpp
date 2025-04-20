2ad5f178527392c93c49aa7c5dc84c2337a14462
unknown <saisinare19@gmail.com>
2025-04-20 08:24:03 +0530

    2Sum Better Solution

--- 3. Array/Easy/2Sum.cpp
+++ 3. Array/Easy/2Sum.cpp
@@ -2,8 +2,8 @@
 using namespace std ;
 class Solution {
 public:
-    vector<int> result;
     vector<int> twoSum(vector<int> &nums,int target){
+        vector<int> result;
         for(int i =0;i<nums.size();i++){
             for(int j =i+1;j<nums.size();j++){
                 if(nums[i] + nums[j] == target){
@@ -15,13 +15,27 @@ public:
         return result;
     }
 
+        vector<int> twoSumBetter(vector<int> &nums,int target){
+            vector<int> result;
+            map<int,int> hash;
+            for(int i=0;i<nums.size();i++){
+                auto it = hash.find(target - nums[i]);
+                if(it != hash.end() ){
+                    result = {i,it->second};
+                    break;
+                }
+                hash[nums[i]] = i;
+            }
+            return result;
+    }
+
 };
 int main() {
     vector<int> nums = {2, 7, 11, 15};  
     int target = 9;
 
     Solution sol;
-    vector<int> result = sol.twoSum(nums, target);
+    vector<int> result = sol.twoSumBetter(nums, target);
 
     if (!result.empty()) {
         cout << "Indices: " << result[0] << ", " << result[1] << endl;
