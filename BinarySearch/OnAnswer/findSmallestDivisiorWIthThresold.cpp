7e3f470028e0b38f7b5e0958732dd480cb6e851f
unknown <saisinare19@gmail.com>
2025-05-23 18:40:42 +0530

    Optimal Solution to find the Smallest divisor with division sum <= threshold

--- BinarySearch/OnAnswer/findSmallestDivisiorWIthThresold.cpp
+++ BinarySearch/OnAnswer/findSmallestDivisiorWIthThresold.cpp
@@ -24,6 +24,30 @@ public:
     }
 };
 
+    int OptimalsmallestDivisor(vector<int>& nums, int threshold) {
+        int high = 0 ;
+        int low = 1;
+        int result = -1;
+        for(int i =0;i<nums.size();i++){
+            if(nums[i]>high){
+                high = nums[i];
+            }
+        }
+        while(low<=high){
+            int mid = (high+low)/2;
+            int sum = 0 ;
+            for(int i =0;i<nums.size();i++){
+                sum = sum + ceil((double)nums[i]/mid);
+            }
+            if(sum<=threshold){
+                high = mid - 1 ;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
 
 int main() {
     Solution sol;
