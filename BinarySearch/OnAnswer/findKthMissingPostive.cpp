003b9338a58c1ce0e4ac43b2382efa2c804580e9
unknown <saisinare19@gmail.com>
2025-05-24 09:42:01 +0530

    finding kth missing number by binary search

---
+++ BinarySearch/OnAnswer/findKthMissingPostive.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+        for(int i =0;i<n;i++){
+            if(arr[i]<=k) k++;
+            else break;
+        }
+        return k;
+    }
+
+    int OptimalfindKthPositive(vector<int>& arr, int k) {
+        int low = 0 ; 
+        int high = arr.size()-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int missing = arr[mid] - (mid+1);
+            if(missing<k) low = mid+1;
+            else high = mid -1;
+        }
+        return low + k;
+    }
+};
+
