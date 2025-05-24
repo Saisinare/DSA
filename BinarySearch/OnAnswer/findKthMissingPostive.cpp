64ca22ca45276ce222234a755aff6f47eef7bba7
unknown <saisinare19@gmail.com>
2025-05-24 16:09:30 +0530

    Find Kth Missing Position

--- BinarySearch/OnAnswer/findKthMissingPostive.cpp
+++ BinarySearch/OnAnswer/findKthMissingPostive.cpp
@@ -18,7 +18,7 @@ public:
         while(low<=high){
             int mid = low + (high-low)/2;
             int missing = arr[mid] - (mid+1);
-            if(missing<k) low = mid+1;
+            if(missing<=k) low = mid+1;
             else high = mid -1;
         }
         return low + k;
