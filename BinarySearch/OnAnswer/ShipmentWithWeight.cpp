a9b37bf8c5d481fa336559b3c89fd3e76c32d064
unknown <saisinare19@gmail.com>
2025-05-27 11:41:37 +0530

    optimal code for getting minimum weight for the shipment to ship the all package within the given day

--- BinarySearch/OnAnswer/ShipmentWithWeight.cpp
+++ BinarySearch/OnAnswer/ShipmentWithWeight.cpp
@@ -41,21 +41,59 @@ public:
         }
     return result;
     }
+
+        int shipWithinDaysOptimal(vector<int>& weights, int days) {
+        int sum = 0;
+        int result = -1;
+        int maxi = INT_MIN;
+    
+        for(int i =0;i<weights.size();i++){
+            sum+= weights[i];
+            if(weights[i]>maxi){
+                maxi = weights[i];
+            }
+        }
+        
+            
+        int high = sum;
+        int low = maxi;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            //checking if the weight is enough
+            int temp = mid;
+            int currentDays = 1;
+            for(int i = 0;i<weights.size();i++){
+                temp -= weights[i];
+                if(temp<0){
+                    currentDays++;
+                    temp = mid - weights[i]; 
+                }
+                if(currentDays>days){
+                    low = mid + 1;
+                    break;
+                }
+            }
+            if(currentDays<=days){
+                result = mid;
+                high = mid-1;
+            }
+        }
+    return result;
+    }
+
 };
 
 int main() {
-    // Example input
     vector<int> weights = {3, 3, 3, 3, 3, 3};
     int days = 2;
-    
-    // Create an object of the Solution class
+
     Solution solution;
-    
-    // Call the shipWithinDays method and store the result
-    int result = solution.shipWithinDays(weights, days);
-    
-    // Output the result
-    cout << "Minimum ship capacity to ship within " << days << " days is: " << result << endl;
-    
+
+    int resultBruteForce = solution.shipWithinDays(weights, days);
+    int resultOptimal = solution.shipWithinDaysOptimal(weights, days);
+
+    cout << "Brute-force result: " << resultBruteForce << endl;
+    cout << "Optimal result: " << resultOptimal << endl;
+
     return 0;
 }
