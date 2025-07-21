2eef041e7696fe5347ed87995c9e14d93903b3ef
unknown <saisinare19@gmail.com>
2025-07-21 22:45:50 +0530

    Optimal solution for printing pascal's triangle

--- 3. Array/Hard/printPascal'sTriangle.cpp
+++ 3. Array/Hard/printPascal'sTriangle.cpp
@@ -21,3 +21,34 @@ public:
         return ans;
     }
 };
+
+//O(n^3);
+
+
+//Optimal solution for printing pascal's triangle 
+class Solution {
+public:
+    int findNum(int row, int col){
+        long long res = 1;
+        for(int i =0;i<col;i++){
+            res *= (row -i);
+            res /= (i+1); 
+        }
+        return res;
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int i =0;i<numRows;i++){
+            vector<int> temp;
+            long long num = 1;
+            temp.push_back(num);
+            for(int j =1;j<=i;j++){
+                num *= (i -j+1);
+                num /= (j);
+                temp.push_back(num);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
