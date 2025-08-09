24e89e6ccf3e4d53e2a5e461a2977f12df68f7d8
unknown <saisinare19@gmail.com>
2025-08-09 22:49:34 +0530

    424. Longest Repeating Character Replacement Brute force

---
+++ slidingWindow/longestSameSubstringWithKReplacement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int maxLen = 0;
+        for(int i =0;i<s.size();i++){
+            int maxF = 0; 
+            vector<int> hash(26,0);
+            //keep the hash table for each element from a to z
+            for(int j =i;j<s.size();j++){
+                //increase the count in the hash according to the letter 
+                hash[s[j]-'A'] ++;
+                //check who has the most frequency and updat it 
+                maxF = max(maxF,hash[s[j]-'A']);
+                //calculate how many characters i have to change so we can get the longest substring with same characters 
+                int changes = (j-i+1) - maxF;
+                //if changes are smaller or equal to the k then update the maxLen
+                if(changes<=k){
+                    maxLen = max(maxLen,j-i+1);
+                }else{
+                    break;
+                }
+            }
+        }
+        return maxLen;
+    }
+};
