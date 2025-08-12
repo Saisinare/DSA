8594c70f123b754794acbd62da53c2101abbed8c
unknown <saisinare19@gmail.com>
2025-08-12 20:51:24 +0530

    optimal for subarray containing ABC sliding window

--- slidingWindow/subArrayContainingABC.cpp
+++ slidingWindow/subArrayContainingABC.cpp
@@ -42,3 +42,22 @@ public:
 //O(N^2)
 //O(N)
 //slightly increase the efficiency but does not clear all of the TLE errors test cases 
+
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        vector<int> lastSeen(3,-1);
+        int count = 0 ;
+        for(int i =0;i<s.size();i++){
+            lastSeen[s[i]-'a'] = i;
+            if(lastSeen[0] != -1 &&lastSeen[1] != -1 &&lastSeen[2] != -1){
+                count +=  (min(lastSeen[0],min(lastSeen[1],lastSeen[2])) +1);
+            }
+        }
+        return count;
+    }
+};
+
+//O(N)
+//O(1) 
