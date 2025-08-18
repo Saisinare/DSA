e3204d9a97b309d9ccb23bd79026ddbe6afb6a03
unknown <saisinare19@gmail.com>
2025-08-18 21:42:57 +0530

    stack using queue

---
+++ stack&Queue/stackUsingQueue.cpp
@@ -0,0 +1,35 @@
+#include <queue>
+using namespace std;
+
+class MyStack {
+public:
+    queue<int> q;
+
+    MyStack() {
+    }
+    
+    void push(int x) {
+        q.push(x);
+        int size = q.size();
+        // Rotate the queue so that new element comes to front
+        for(int i = 0; i < size - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
