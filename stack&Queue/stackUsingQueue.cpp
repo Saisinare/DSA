bacb61345e5559fb495f44ed64a1d951e1532c9b
unknown <saisinare19@gmail.com>
2025-08-19 22:37:32 +0530

    stack using queue redone

--- stack&Queue/stackUsingQueue.cpp
+++ stack&Queue/stackUsingQueue.cpp
@@ -1,35 +1,43 @@
-#include <queue>
-using namespace std;
-
 class MyStack {
 public:
-    queue<int> q;
-
+    queue<int> q1;
+    queue<int> q2;
     MyStack() {
+        
     }
     
-    void push(int x) {
-        q.push(x);
-        int size = q.size();
-        // Rotate the queue so that new element comes to front
-        for(int i = 0; i < size - 1; i++) {
-            q.push(q.front());
-            q.pop();
+    void push(int x) { //O(N)
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        q1.push(x);
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
         }
     }
     
-    int pop() {
-        int val = q.front();
-        q.pop();
-        return val;
+    int pop() {//O(1)
+        int data = q1.front();
+        q1.pop();
+        return data;
     }
     
-    int top() {
-        return q.front();
+    int top() {//O(1)
+        return q1.front();
     }
     
-    bool empty() {
-        return q.empty();
+    bool empty() {//O(1)
+        return q1.empty();
     }
 };
 
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
