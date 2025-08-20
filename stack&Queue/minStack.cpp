7f5f9a07b504a0dc02c47507d2434515c865d036
unknown <saisinare19@gmail.com>
2025-08-20 19:54:13 +0530

    min stack optimal approach '

--- stack&Queue/minStack.cpp
+++ stack&Queue/minStack.cpp
@@ -40,3 +40,52 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+
+class MinStack {
+public:
+    stack<long long> st;
+    long long mini;
+
+    MinStack() {
+        mini = LLONG_MAX;
+    }
+
+    void push(int val) {
+        if(st.empty()) {
+            st.push(val);
+            mini = val;
+        } else {
+            if(val >= mini) {
+                st.push(val);
+            } else {
+                st.push(2LL * val - mini); // encode
+                mini = val;
+            }
+        }
+    }
+
+    void pop() {
+        if(st.empty()) return;
+        long long n = st.top();
+        st.pop();
+        if(n < mini) {
+            // n was encoded, restore previous min
+            mini = 2 * mini - n;
+        }
+        if(st.empty()) mini = LLONG_MAX; // reset if empty
+    }
+
+    int top() {
+        if(st.empty()) return -1;
+        long long n = st.top();
+        if(n >= mini) return (int)n;
+        else return (int)mini; // encoded marker → actual top is current min
+    }
+
+    int getMin() {
+        return (int)mini;
+    }
+};
+
+//optimal solution without using pair in stack 
+//eachc operation usage O(1) complexity
