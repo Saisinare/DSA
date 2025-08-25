36ec611201a9ca1a823cfe7cd0587ab7a4066409
unknown <saisinare19@gmail.com>
2025-08-25 14:45:28 +0530

    subarray rang sum

---
+++ stack&Queue/astroidCollision.cpp
@@ -0,0 +1,29 @@
+//Direct Optimal 
+    /*
+        1. make an stack or an array with ans 
+        2. loop through the asteroids array
+        3. insert the positive element directly with no condition 
+        4. if negative number occurs 
+            1. while(we have the number which is positive and our abs negative val is greater than this element pop the element)
+            2. if(their are elements remaining which is equal to the astestroids[i] and its sign is positive then ) remove that elemnt 
+            3. else if(ans is empty or ans's last element is negative) insert the asetroid in ans.
+    */
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> ans;
+        int n = asteroids.size();
+        for(int i =0;i<n;i++){
+            if(asteroids[i]>0) ans.push_back(asteroids[i]);
+            else{
+                while(!ans.empty() && ans.back()>0 && ans.back() < abs( asteroids[i] ) ) ans.pop_back();
+                if(!ans.empty() && ans.back() == abs(asteroids[i])) ans.pop_back();
+                else if(ans.empty() || ans.back()<0) ans.push_back(asteroids[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+//O() T.C
+//O() S.C
---
+++ stack&Queue/subarrayRangeSum.cpp
@@ -0,0 +1,27 @@
+//brute force 
+    /*
+        1. Generate all of subarrays using nested loop
+        2. while looping in the array keep track of max an min in the subarray 
+        3. concurrently add this max - min in the sum
+        4. return sum
+    */
+class Solution {
+public:
+
+    long long subArrayRanges(vector<int>& nums) {
+        long long sum = 0;
+        for(int i =0;i<nums.size();i++){
+            int maxi = INT_MIN ,  mini = INT_MAX;
+            for(int j =i;j<nums.size();j++){
+                cout<<sum;
+                maxi = max(maxi,nums[j]);
+                mini = min(mini,nums[j]);
+                sum = sum +  (maxi - mini);
+            }
+        }
+        return sum;
+    }
+};
+
+//O(N^2) T.C
+//O(1) S.C.
