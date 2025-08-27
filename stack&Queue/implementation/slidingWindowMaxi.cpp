a9a111953fd7c4d4a73c43a63152a6a701fbb775
unknown <saisinare19@gmail.com>
2025-08-27 09:41:43 +0530

    sliding windows maximum optimal

--- stack&Queue/implementation/slidingWindowMaxi.cpp
+++ stack&Queue/implementation/slidingWindowMaxi.cpp
@@ -7,6 +7,9 @@
         T.C. - n^2
         S.C. - n
     */
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
@@ -18,6 +21,33 @@ public:
             }
             ans.push_back(maxi);
         }      
+        return ans;
+    }
+};
+
+
+//Optimal Approach 
+    /*
+        use double ended queue 
+        deque<int> 
+    */
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> q; 
+        for(int i =0;i<nums.size();i++){
+            if(!q.empty() && q.front()<=i-k){
+                q.pop_front();
+            }
+            while(!q.empty() && nums[q.back()] <= nums[i]){
+                q.pop_back();
+            }
+            q.push_back(i);
+            if(i>=k-1){
+                ans.push_back(nums[q.front()]);
+            }
+        }
         return ans;
     }
 };
