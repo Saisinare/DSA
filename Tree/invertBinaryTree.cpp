e46e99c18787a7075ce35e6cdf03c112160e83f1
unknown <saisinare19@gmail.com>
2025-09-12 11:44:30 +0530

    invert Binary Tree

---
+++ Tree/invertBinaryTree.cpp
@@ -0,0 +1,20 @@
+//swap the right and left node of root 
+//repeat this until the end of tree using recursion 
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == nullptr) return root;
+
+        //swap the left and right nodes 
+        TreeNode* temp = root->left;
+        root->left = root->right;
+        root->right = temp;
+
+        //recursively call inertTree to left and right 
+        invertTree(root->right);
+        invertTree(root->left);
+
+        return root;
+    }
+};
