c90c5858361f0a92dfa84dcffb08b44d7c98bd6d
unknown <saisinare19@gmail.com>
2025-09-13 08:58:35 +0530

    diameter of binary tree optimal solution

--- Tree/diameterOfBinaryTree.cpp
+++ Tree/diameterOfBinaryTree.cpp
@@ -1,4 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+//Brute force 
+/*
+    T.C. O(N^2)
+    S.C. O(1)
+ */
 class Solution {
 public:
     int height(TreeNode* root){
@@ -14,5 +21,28 @@ public:
         int rightDiameter = diameterOfBinaryTree(root->right);
         int currentHeight = height(root->left) + height(root->right);
         return max(currentHeight,max(leftDiameter,rightDiameter));
+    }
+};
+
+//optimal solution 
+/*
+we will remove the calling of height function in diameter function separately
+ */
+
+class Solution {
+public:
+    int ans = 0;
+    int height(TreeNode* root){
+        if(root == nullptr) return 0;
+
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+        ans = max(ans,leftHeight+rightHeight);
+        return max(leftHeight,rightHeight)+1;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        height(root);
+        return ans;
+
     }
 };
