2cca71012f898145b1104215b38a0469bc3f5a89
unknown <saisinare19@gmail.com>
2025-09-14 17:08:26 +0530

    balance binary tree optimal

--- Tree/balancedTree.cpp
+++ Tree/balancedTree.cpp
@@ -25,3 +25,28 @@ public:
 };
 
 //Brute force with TC O^2
+
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(root==nullptr) return true;
+        int lH = height(root->left);
+        int rH = height(root->right);
+        if(lH == -1 || rH == -1) return -1;
+        if(abs(lH-rH)>1) return -1;
+        return 1 + max(lH,rH);  
+    }
+    bool isBalanced(TreeNode* root) {
+        if (root==nullptr) return true;
+
+        int lh = height(root->left);
+        int rh = height(root->right);
+        if(lh == -1 || rh==-1) return false;
+        if(abs(lh-rh)>1) return false;
+
+        return true;
+
+    }
+};
+
+//optimal solution of O(N)
