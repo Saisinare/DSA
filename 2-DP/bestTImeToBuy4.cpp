4ec2a63f3f65e38616dbbbf7aa46f2dd5af201c5
unknown <saisinare19@gmail.com>
2025-11-03 13:41:40 +0530

    best time to buy and sell stocks with fees

--- 2-DP/bestTImeToBuy4.cpp
+++ 2-DP/bestTImeToBuy4.cpp
@@ -72,7 +72,137 @@ public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
         vector<vector<int>> dp (n+1,vector<int>(2,-1));
-        //base case 
+        //base case //Brute force: Recursion 
+/*
+class Solution {
+public:
+    int solve(int k, vector<int>& prices,int index,int n,int buy){
+        if(k == 0 || index >= n) return 0;
+        int profit = 0 ;
+        if(buy){
+            profit = max(
+                        -prices[index] + solve(k,prices,index+1,n,0),
+                        solve(k,prices,index+1,n,1)
+                        );
+        }else{
+            profit = max(
+                        prices[index] + solve(k-1,prices,index+1,n,1),
+                        solve(k,prices,index+1,n,0)
+            );
+        }
+        return profit;
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        return solve(k,prices,0,prices.size(),1);
+    }
+};
+*/
+//Use memoization to bypass the TLE 
+/*
+class Solution {
+public:
+    int solve(int k, vector<int>& prices,int index,int n,int buy,vector<vector<vector<int>>>& dp){
+        if(k == 0 || index >= n) return 0;
+        int profit = 0 ;
+        if(dp[index][buy][k]!=-1) return dp[index][buy][k]; 
+        if(buy){
+            profit = max(
+                        -prices[index] + solve(k,prices,index+1,n,0,dp),
+                        solve(k,prices,index+1,n,1,dp)
+                        );
+        }else{
+            profit = max(
+                        prices[index] + solve(k-1,prices,index+1,n,1,dp),
+                        solve(k,prices,index+1,n,0,dp)
+            );
+        }
+        return dp[index][buy][k] =  profit;
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        return solve(k,prices,0,n,1,dp);
+    }
+};
+*/
+/*
+    TC: O(N*k*2)
+    SC: O(N*k*2) + O(N)
+*/
+
+//Use tabulation: to reduce the space complexity and remove the recursive stack space 
+/*
+    1. Find out the base case 
+    2. Fill the predefined values 
+    3. Fill out rest of the array 
+*/
+/*
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        // base case: when index == 0 or count == k it will be 0; 
+        for(int i = 0;i<2;i++){
+            for(int j = 0;j<k;j++){
+                dp[0][i][j] = 0;
+            }
+        }
+        for(int index = n-1;index>=0;index--){
+            for(int buy = 0;buy<2;buy++ ){
+                for(int count = 1;count<=k;count++){
+                        if(buy){
+                            dp[index][buy][count] = max(
+                                        -prices[index] + dp[index+1][0][count],
+                                        dp[index+1][1][count]
+                                        );
+                        }else{
+                            dp[index][buy][count] = max(
+                                        prices[index] + dp[index+1][1][count-1],
+                                        dp[index+1][0][count]
+                            );
+                        }
+                }
+            }
+        }
+        return dp[0][1][k];
+    }
+};
+*/
+/*
+    TC : O(N*K*2)
+    SC : O(N*K*2) 
+*/
+
+//to optimize this SC more we can only use two variable array 
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> cur(2,vector<int>(k+1,0));
+        vector<vector<int>> ahead(2,vector<int>(k+1,0));
+        // base case: when index == 0 or count == k it will be 0; 
+        for(int index = n-1;index>=0;index--){
+            for(int buy = 0;buy<2;buy++ ){
+                for(int count = 1;count<=k;count++){
+                        if(buy){
+                            cur[buy][count] = max(
+                                        -prices[index] + ahead[0][count],
+                                        ahead[1][count]
+                                        );
+                        }else{
+                            cur[buy][count] = max(
+                                        prices[index] + ahead[1][count-1],
+                                        ahead[0][count]
+                            );
+                        }
+                }
+            }
+            ahead = cur;
+        }
+        return ahead[1][k];
+    }
+};
         for(int buy= 0 ;buy<2;buy++){
             dp[n][buy] = 0;  
         }
