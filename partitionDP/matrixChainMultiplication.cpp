ff121205f4b0875012194d13ed6e57cf496b406f
unknown <saisinare19@gmail.com>
2025-11-06 23:05:07 +0530

    matrix chain multiplication

---
+++ partitionDP/matrixChainMultiplication.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int solve(vector<int> &arr,int i , int j,vector<vector<int>>& dp){
+        if(i==j) return 0;
+        int mini = 1e9 ; 
+        int ans = 0 ;
+        if(dp[i][j] != -1 ) return dp[i][j];
+        for(int k = i;k<j;k++){
+            ans = (arr[i-1] * arr[k] * arr[j]) + 
+            solve(arr,i,k,dp) + 
+            solve(arr,k+1,j,dp) ;
+            mini = min(mini,ans);
+        }
+        return dp[i][j] = mini;
+    }
+    int matrixMultiplication(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solve(arr,1,n-1,dp);
+    }
+};
