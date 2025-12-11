0878f624630fc10c1e10112ec448c5c112820c94
unknown <saisinare19@gmail.com>
2025-12-11 10:17:21 +0530

    resolve

--- 8-Tree/BSTTraversals/inorderIterative.cpp
+++ 8-Tree/BSTTraversals/inorderIterative.cpp
@@ -1,14 +1,7 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+  
+
+ 
  /*
  Start at root and set node = root.
 
@@ -34,8 +27,19 @@ stack is not empty OR
 
 node is not NULL.
  */
+#include <bits/stdc++.h>    
+using namespace std;
 class Solution {
 public:
+
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
     vector<int> inorderTraversal(TreeNode* root) {
         if(root==nullptr) return {};
         stack<TreeNode*> st;
