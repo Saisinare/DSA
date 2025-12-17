e711d11e19f870ab81ec9244b4f51bfa2148b344
unknown <saisinare19@gmail.com>
2025-12-17 19:04:32 +0530

    morris preorder

---
+++ 8-Tree/hardProblems/morrisPreorder.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    TreeNode* findPredecessor(TreeNode* root,TreeNode* curr){
+        //if we do not put (root->right != curr) 
+        //this loop will continue till infinite so we have to check if we just arrive at the same point 
+        while(root->right!=nullptr && root->right != curr){
+            root = root->right;
+        }
+        return root;
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root == nullptr) return {};
+        TreeNode* curr = root;
+        vector<int> ans;
+        while(curr!=nullptr){
+            if(curr->left == nullptr){
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else {
+                TreeNode* pred = findPredecessor(curr->left,curr);  
+                if(pred->right == nullptr){
+                    ans.push_back(curr->val);
+                    pred->right = curr;
+                    curr = curr->left;
+                }else{
+                    pred->right = nullptr;
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
