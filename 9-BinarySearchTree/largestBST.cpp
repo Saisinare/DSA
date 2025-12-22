3b896f38eefddf99340063aa57c84f353b122acb
unknown <saisinare19@gmail.com>
2025-12-22 10:37:06 +0530

    largest BST

---
+++ 9-BinarySearchTree/largestBST.cpp
@@ -0,0 +1,39 @@
+/* Tree node structure  used in the program
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+
+    /*You are required to complete this method */
+    // Return the size of the largest sub-tree which is also a BST
+    
+    //{size,min,max}
+    vector<int> findLargestWithMinMax(Node *root){
+        if(root==nullptr) return {0,INT_MAX,INT_MIN};
+        
+        auto left = findLargestWithMinMax(root->left);
+        auto right = findLargestWithMinMax(root->right);
+        
+        //left[max] && right[min]
+        if(left[2] < root->data && right[1] > root->data){
+            return {1 + left[0] + right[0],
+                    min(left[1],root->data),
+                    max(right[2],root->data)
+                    };
+        }
+        return {max(right[0],left[0]),INT_MIN,INT_MAX};
+    }
+    int largestBst(Node *root) {
+        return findLargestWithMinMax(root)[0];
+    }
+};
