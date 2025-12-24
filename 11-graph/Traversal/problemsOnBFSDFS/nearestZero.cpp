d8ce9b4ac5b1868de2a187bcbb961dc2dcb983e0
unknown <saisinare19@gmail.com>
2025-12-24 13:25:54 +0530

    nearest zeros in graph

---
+++ 11-graph/Traversal/problemsOnBFSDFS/nearestZero.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void traverse(vector<vector<int>>& mat,int m,int n,pair<int,int> node){
+        queue<pair<int,int>> q;
+        q.push(node);
+        int dist = 0;
+        int nodeI = node.first;
+        int nodeII = node.second;
+        vector<vector<bool>> visited(m, vector<bool>(n, false)); 
+        visited[nodeI][nodeII] = true;
+
+        while(!q.empty()){
+            int size = q.size();
+            dist++;
+        
+            for(int k =0;k<size;k++){
+                pair<int,int> front = q.front();
+                int i = front.first; 
+                int j = front.second;
+
+                q.pop();
+                //left 
+                if(j>0 && !visited[i][j-1]){
+                    if (mat[i][j-1] == 0){
+                        mat[nodeI][nodeII] = dist;
+                        return;
+                    }
+                    q.push({i,j-1});
+                    visited[i][j-1] = true;
+                }
+                //right 
+                if(j<n-1 && !visited[i][j+1]){
+                    if (mat[i][j+1] == 0){
+                        mat[nodeI][nodeII] = dist;
+                        return;
+
+                    }
+                    q.push({i,j+1});
+                    visited[i][j+1] = true;
+                }
+                //up 
+                if(i>0 && !visited[i-1][j]){
+                    if (mat[i-1][j] == 0 ){
+                        mat[nodeI][nodeII] = dist;
+                        return;
+                    }
+                    q.push({i-1,j});
+                    visited[i-1][j] = true;
+                }
+                //down 
+                if(i<m-1 && !visited[i+1][j]){
+                    if (mat[i+1][j] == 0){
+                        mat[nodeI][nodeII] = dist;
+                        return;
+                    }
+                    q.push({i+1,j});
+                    visited[i+1][j] = true;
+                }
+            }
+        }
+    }
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        if(mat.empty()) return {};
+        int m = mat.size();
+        int n = mat[0].size();
+        //check where is 1 from that point start BFS traversing
+        for(int i=0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(mat[i][j] == 1){
+                    traverse(mat,m,n,{i,j});
+                }
+            }
+        }
+
+        return mat;
+    }
+};
