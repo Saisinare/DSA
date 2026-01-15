de8ef0bbed8210b49fe606947f8ad9e30847bc0e
unknown <saisinare19@gmail.com>
2026-01-15 10:43:02 +0530

    rotten oranges upate

--- 11-graph/problemsOnBFSDFS/rottenOranges.cpp
+++ 11-graph/problemsOnBFSDFS/rottenOranges.cpp
@@ -1,76 +1,79 @@
-//Push the position of rotten oranges in queue 
-//Run a while loop till q.empty 
-//Now pop elements out one by one but when you pop out insert its neibours who's value is 1  
-//and each time increase the minute count by 1 
-//return minute count 
 
-//(when to stop) what is the base case ?? 
-//their is no neibours who is 1 
-class Solution {
+
+class Solution{
 public:
-    int bfs(vector<vector<int>>& grid,queue<pair<int,int>>& rotten){
-        int min = 0;
-        while(!rotten.empty()){
-            int size = rotten.size();
-            //insert the 4D neibours
-            for(int k =0;k<size;k++){
-                auto front = rotten.front();
-                rotten.pop();
-                int i = front.first;
-                int j = front.second;
-                if(i<grid.size()-1 && grid[i+1][j] == 1){
-                    grid[i+1][j] = 2;
-                    rotten.push({i+1,j});
+    int dfs(vector<vector<int>> &grid,int m ,int n,queue<pair<int,int>>& q,int& fresh){
+        vector<int> dx = {1,-1,0,0};
+        vector<int> dy = {0,0,1,-1};
+        int count = 0 ;
+        while(!q.empty()){
+            int size = q.size();
+            for(int i =0;i<size;i++){
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
+                for(int k =0;k<4;k++){
+                    int nr = row + dx[k];
+                    int nc = col + dy[k];
+                    if(nr>=0 && nr<m && nc>=0 && nc <n && grid[nr][nc] == 1){
+                        grid[nr][nc] = 2;
+                        q.push({nr,nc});
+                        fresh--;
+                    } 
                 }
-                if(i>0 && grid[i-1][j] == 1){
-                    grid[i-1][j] = 2;
-                    rotten.push({i-1,j});
-                }
-                if(j<grid[0].size()-1 && grid[i][j+1] == 1){
-                    grid[i][j+1] = 2;
-                    rotten.push({i,j+1});
-                }
-                if(j>0 && grid[i][j-1] == 1){
-                    grid[i][j-1] = 2;
-                    rotten.push({i,j-1});
-                }   
-            }         
-            //complete
-            if(!rotten.empty()) min++;
+            }
+            if(!q.empty()) count++;
         }
-        return min;
+    return count;
     }
-    int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int,int>> rotten;
+    int orangesRotting(vector<vector<int>> &grid) {
         int m = grid.size();
         int n = grid[0].size();
-        bool hasRotten = false;
-        bool isEmpty = true;
-        for(int i =0;i<m;i++){
-            for(int j =0;j<n;j++){
-                if(grid[i][j] == 2){
-                    rotten.push({i,j});
-                    hasRotten = true;
-                }
-                if(grid[i][j] == 1){
-                    isEmpty = false;
-                }
-            }
-        }
-        if(isEmpty) return 0;
-        if(!hasRotten) return -1;
-        int min = bfs(grid,rotten);
-        for(int i =0;i<m;i++){
-            for(int j =0;j<n;j++){
-                if(grid[i][j] == 1){
-                    return -1;
-                }
-            }
-        }
+        queue<pair<int,int>> q;
+        //O(M*N)
+        int fresh = 0;
+        bool hasRotten = false ;
 
-        return min;
+        //O(m*n)
+
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(grid[i][j] == 1){
+                    fresh++;
+                }
+                if(grid[i][j] == 2){
+                    hasRotten = true;
+                    q.push({i,j});
+                }
+            }
+        }
+        // if their is no fresh oranges then time is 0 
+        if(fresh==0) return 0;
+        // if their is no rotten oranges then it is not possible to to rotten entire grid so return -1 
+        if(!hasRotten) return -1;
+
+        // do dfs and if after that if their is even a single fresh orange then return -1 
+        //O(m*n)
+
+        int count = dfs(grid,m,n,q,fresh);
+        // fresh = 0 ;
+        //O(M*N)
+        // for(int i =0;i<m;i++){
+        //     for(int j =0;j<n;j++){
+        //         if(grid[i][j] == 1){
+        //             fresh++;
+        //         }
+        //     }
+        // }
+        //instead of this just pass the fresh count to dfs and decrease it when you rotten something 
+        if(fresh != 0) return -1;
+        return count;
 
     }
 };
 
-//
+//Time complexity:  O(M*N)
+//Space complexity: O(M*N)
+
+
+
