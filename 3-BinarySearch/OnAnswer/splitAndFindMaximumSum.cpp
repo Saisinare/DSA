29032cf98101f206b05b46037ec8545150375dc2
unknown <saisinare19@gmail.com>
2026-02-03 19:53:58 +0530

    split and find max sum

--- 3-BinarySearch/OnAnswer/splitAndFindMaximumSum.cpp
+++ 3-BinarySearch/OnAnswer/splitAndFindMaximumSum.cpp
@@ -1,58 +1,36 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int findStudentCount(vector<int>& arr,int n,int pages){
-    int currentPages = 0;
-    int studentCount = 1;
-    for(int i =0;i<n;i++){
-        if(arr[i]+currentPages<=pages){
-            currentPages+= arr[i];
-        }else{
-            studentCount++;
-            currentPages = arr[i];
-        }
-    }
-    return studentCount;    
-}
-
-int findPages(vector<int>& arr, int n, int m) {
-    if(m>n) return -1;
-        int maxi = INT_MIN;
-    int sum = 0;
-    for(int i =0;i<arr.size();i++){
-        sum += arr[i];
-        if(arr[i]>maxi){
-            maxi = arr[i];
-        }
-    }
-
-    int low = maxi;
-    int high = sum;
-
-    while(low<=high){
-        int mid = low + (high-low)/2;
-        int studentCount = findStudentCount(arr,n,mid);
-        if(studentCount>m) low = mid+1;
-        else high = mid-1;
-    }
-    return low;
-}
 class Solution {
 public:
-    int splitArray(vector<int>& nums, int k) {
-        int result = findPages(nums,nums.size(),k);
-        return result;
+    int splitArrayAndReturnSplitCount(vector<int> &a,int k){
+        int count = 1;
+        int sum = 0;
+        for(int i =0;i<a.size();i++){
+            if(sum+a[i]<=k){
+                sum+=a[i];
+            }else{
+                sum = a[i];
+                count ++;
+            }
+        }
+        return count;
+    }
+    int largestSubarraySumMinimized(vector<int> &a, int k) {
+        int low = *max_element(a.begin(),a.end());
+        int high = accumulate(a.begin(),a.end(),0);
+        if(k>a.size()) return -1;
+        int ans = -1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int count = splitArrayAndReturnSplitCount(a,mid);
+            if(count <= k){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 };
 
-
-int main() {
-    vector<int> nums = {7, 2, 5, 10, 8};  // Example array
-    int k = 2;  // Number of subarrays (students)
-
-    Solution sol;
-    int result = sol.splitArray(nums, k);
-    cout << "Minimum largest sum after split: " << result << endl;
-
-    return 0;
-}
+// if mid increases : number of subarrays (count) decreases
+// if mid decreases : number of subarrays (count) increases
