545de02100b73a6ff327a94dc414b39fb6678bca
unknown <saisinare19@gmail.com>
2026-02-04 16:26:57 +0530

    find peak element

--- 3-BinarySearch/On2DArray/findPeakElement.cpp
+++ 3-BinarySearch/On2DArray/findPeakElement.cpp
@@ -1,53 +1,40 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-
+//find the peak row with max element column wise 
+//see if the right and left of that element is smaller if yes then return this row and mid 
+//if left half is greater than the element then answer is in the left half 
+//or answer is in the right half 
 class Solution {
 public:
-    vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int low = 0;
-        int high = mat[0].size()-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            int eleIndex = findMax(mat,mid,mat.size());
-            int left = (mid-1>=0)?mat[eleIndex][mid-1]:-1;
-            int right = (mid+1<mat[eleIndex].size())?mat[eleIndex][mid+1]:-1;
-            if(mat[eleIndex][mid]>left && mat[eleIndex][mid]>right) return {eleIndex,mid};
-            if(mat[eleIndex][mid]<left){
-                high = mid -1;
-            }else if(mat[eleIndex][mid]<right){
-                low = mid + 1;
+    int findMaxElementIndex(vector<vector<int>>& mat,int col){
+        int m = mat.size();
+        int maxi = INT_MIN;
+        int idx = -1;
+        for(int i = 0;i<m;i++){
+            if(mat[i][col] > maxi){
+                maxi = mat[i][col]; 
+                idx = i;
             }
+        }
+        return idx;
+    }
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int low = 0 ;
+        int high = m -1;
+        while(low <= high){
+            int mid = low + (high - low )/2;
+            int row = findMaxElementIndex(mat,mid);
+
+            int left = mid-1>=0? mat[row][mid-1] : INT_MIN;
+            int right = mid +1 <m ? mat[row][mid+1]: INT_MIN;
+
+            if(mat[row][mid]>left && mat[row][mid]>right ) return {row,mid};
+            else if(mat[row][mid]<left) high = mid -1;
+            else low = mid +1;
         }
         return {};
     }
-    int findMax(vector<vector<int>>&arr,int mid,int m){
-        int maxi = INT_MIN;
-        int rowIndex = -1;
-        for(int i = 0;i<m;i++){
-            if(arr[i][mid]>maxi){ 
-                maxi = arr[i][mid];
-                rowIndex = i;
-            }
-        }
-        return rowIndex;
-    }
 };
 
-
-
-int main() {
-    Solution sol;
-    vector<vector<int>> mat = {
-        {10, 8, 10, 10},
-        {14, 13, 12, 11},
-        {15, 9, 11, 21},
-        {16, 17, 19, 20}
-    };
-
-    vector<int> result = sol.findPeakGrid(mat);
-    cout << "Peak found at: [" << result[0] << ", " << result[1] << "]" << endl;
-
-    return 0;
-}
+//Time Complexity: O(N Log M)
+//Space Complexity: O(1)
