af7a0b37c25649da4f49aff66dd24b2891933d68
unknown <saisinare19@gmail.com>
2026-02-04 16:55:46 +0530

    median in matrix

--- 3-BinarySearch/On2DArray/findMedian.cpp
+++ 3-BinarySearch/On2DArray/findMedian.cpp
@@ -1,71 +1,58 @@
-#include<bits/stdc++.h>
-using namespace std;
+//Write a function which will calculate the total number of element in matrix which is smaller than equal to the element 
+//find upper bound of each row 
+//add this upper bound count to the final count 
+//if we found that the total smaller or equal elements are == m*n/2 then return it or 
 
-
-int findMax(vector<vector<int>> &matrix){
-    int maxi = INT_MIN;
-    for(int i =0;i<matrix.size();i++){
-        if(matrix[i][matrix[0].size()-1]>maxi){
-            maxi=matrix[i][matrix[0].size()-1];
-        }
-    }
-    return maxi;
-}
-int findMin(vector<vector<int>> &matrix){
-    int mini = INT_MAX;
-    
-
-    for(int i =0;i<matrix.size();i++){
-        if(matrix[i][0]<mini){
-            mini=matrix[i][0];
-        }
-    }
-    return mini;
-}
-int findNoOfSmallerElements(vector<vector<int>> &matrix,int target){
-    int sum = 0;
-
-    for(int i =0;i<matrix.size();i++){
-    int low = 0;
-    int high = matrix[i].size()-1;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(matrix[i][mid]<=target){
-                low = mid +1 ;
+class Solution{
+public:
+    int findUpperBound(vector<vector<int>>&matrix,int row,int target){
+        int low = 0 ;
+        int high = matrix[row].size()-1;
+        int ans = matrix[row].size();
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(matrix[row][mid] > target){
+                ans = mid;
+                high = mid -1;
             }else{
-                high = mid -1 ;
+                low = mid +1;
             }
         }
-        sum += low;
+        return ans;
     }
-    return sum ;
-}
-int getMedian(vector<vector<int>> &matrix)
-{
-    int n = matrix.size();
-    int m = matrix[0].size();
-
-    int low = findMin(matrix);
-    int high = findMax(matrix);
-    while(low<=high){
-        int mid = low + (high-low)/2;
-        int smallerElements = findNoOfSmallerElements(matrix,mid);
-        int required = (n*m+1)/2;
-        if(smallerElements<required){
-            low = mid +1 ;
-        }else{
-            high = mid -1;
+    int countSmallerOrEqual(vector<vector<int>>&matrix,int target){
+        int count = 0 ;
+        for(int i=0;i<matrix.size();i++){
+            count += findUpperBound(matrix,i,target);
         }
+        return count;
     }
-    return low;
-}
-int main() {
-    vector<vector<int>> matrix = {
-        {1, 3, 5},
-        {2, 6, 9},
-        {3, 6, 9}
-    };
+    int findMedian(vector<vector<int>>&matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int low = INT_MAX;
+        int high = INT_MIN;
+        int req = (m*n)/2;
+        int ans = 0;
+        for(int i =0;i<m;i++){
+            low = min(low,matrix[i][0]);
+            high = max(high,matrix[i][n-1]);
+        }
 
-    cout << "Median is: " << getMedian(matrix) << endl;
-    return 0;
-}
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int smallEqual = countSmallerOrEqual(matrix,mid);
+            if(smallEqual > req){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid +1 ;
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
